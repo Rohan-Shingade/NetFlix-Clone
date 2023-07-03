038b96dc2e8894a0e6a817dc2c939d76fbe00efa
CarryAllen <rohanshingade1095@gmail.com>
2023-07-03 17:48:33 +0530

    script added

---
+++ script.js
@@ -0,0 +1,12 @@
+const label = document.querySelector('.label');
+const content = document.querySelector('.content');
+
+// label.addEventListener('click',() => {
+//   content.classList.toggle('content');
+// });
+
+// if(content.classList.contains('hidden')) {
+//   content.classList.remove('hidden');
+// } else {
+//   content.classList.add('hidden') ;
+// }
